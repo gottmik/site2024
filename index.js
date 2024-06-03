898f4f1f12a64a9aafa3063cc9d774d64bf02e9f
Marcela Fróes <91344634+gottmik@users.noreply.github.com>
2024-06-03 10:52:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,8 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     var index = 0;
-//     var images = document.querySelectorAll(".banner img");
-//     var interval = setInterval(changeImage, 3000); // Altera a imagem a cada 3 segundos
-
-//     function changeImage() {
-//         // Oculta a imagem atual
-//         images[index].classList.remove("active");
-//         // Incrementa o índice
-//         index++;
-//         // Se chegarmos ao final, voltamos para o início
-//         if (index === images.length) {
-//             index = 0;
-//         }
-//         // Mostra a próxima imagem
-//         images[index].classList.add("active");
-//     }
-// });
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     var texts = document.querySelectorAll(".rotating-text .text");
-//     var index = 0;
-//     var interval = setInterval(changeText, 3000); // Altera o texto a cada 3 segundos
-
-//     function changeText() {
-//         // Oculta o texto atual
-//         texts[index].classList.remove("active");
-//         // Incrementa o índice
-//         index++;
-//         // Se chegarmos ao final, voltamos para o início
-//         if (index === texts.length) {
-//             index = 0;
-//         }
-//         // Mostra o próximo texto
-//         texts[index].classList.add("active");
-//     }
-// });
-
 document.addEventListener("DOMContentLoaded", function() {
     var texts = document.querySelectorAll(".rotating-content .text");
     var index = 0;
-    var interval = setInterval(changeContent,5000); // Altera o conteúdo a cada 3 segundos
+    var images = ["cargaimed.png", "implantedent.png", "guiada.png"]; // Array com os caminhos das imagens
+    var rotatingContent = document.querySelector(".rotating-content");
 
     function changeContent() {
         // Oculta o texto atual
@@ -55,8 +17,13 @@ document.addEventListener("DOMContentLoaded", function() {
         texts[index].classList.add("active");
 
         // Troca a imagem de fundo da div
-        var images = ["cargaimed.png", "implantedent.png", "guiada.png"]; // Array com os caminhos das imagens
-        var rotatingContent = document.querySelector(".rotating-content");
         rotatingContent.style.backgroundImage = "url(" + images[index] + ")";
     }
+
+    // Inicializa o conteúdo imediatamente
+    texts[index].classList.add("active");
+    rotatingContent.style.backgroundImage = "url(" + images[index] + ")";
+
+    // Define o intervalo para trocar o conteúdo
+    var interval = setInterval(changeContent, 5000);
 });
