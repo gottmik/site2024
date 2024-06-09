51063ab35d5da0e3b4c8c56b1c33420cb6e39254
Marcela Fróes <91344634+gottmik@users.noreply.github.com>
2024-06-09 10:53:53 -0300

    mudança do container 1 para prox e anterior

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var texts = document.querySelectorAll(".rotating-content .text");
-    var index = 0;
-    var images = ["cargaimed.png", "implantedent.png", "guiada.png"]; // Array com os caminhos das imagens
-    var rotatingContent = document.querySelector(".rotating-content");
+let currentIndex = 0;
+        const items = document.querySelectorAll('.carousel-item');
 
-    function changeContent() {
-        // Oculta o texto atual
-        texts[index].classList.remove("active");
-        // Incrementa o índice
-        index++;
-        // Se chegarmos ao final, voltamos para o início
-        if (index === texts.length) {
-            index = 0;
+        function showSlide(index) {
+            items.forEach((item, i) => {
+                item.classList.toggle('active', i === index);
+            });
         }
-        // Mostra o próximo texto
-        texts[index].classList.add("active");
 
-        // Troca a imagem de fundo da div
-        rotatingContent.style.backgroundImage = "url(" + images[index] + ")";
-    }
+        function nextSlide() {
+            currentIndex = (currentIndex + 1) % items.length;
+            showSlide(currentIndex);
+        }
 
-    // Inicializa o conteúdo imediatamente
-    texts[index].classList.add("active");
-    rotatingContent.style.backgroundImage = "url(" + images[index] + ")";
-
-    // Define o intervalo para trocar o conteúdo
-    var interval = setInterval(changeContent, 5000);
-});
+        function prevSlide() {
+            currentIndex = (currentIndex - 1 + items.length) % items.length;
+            showSlide(currentIndex);
+        }
